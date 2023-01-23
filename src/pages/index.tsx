314f0bdb027ba8f0f55922af8facd9799d24151a
Kened Silva Felix <ksfelix1@stefanini.com>
2023-01-23 18:56:18 -0300

    Consumindo api do Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,28 @@
+// import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from './home.module.scss'
 import { SubscribeButton } from '@/components/SubscribeButton'
+import { stripe } from '@/services/stripe';
 
-const inter = Inter({ subsets: ['latin'] })
+// Client Side
+//Server Side
+//Static SiteGeneration
 
-export default function Home() {
+//Post do Blog
+
+//Conteudo (SSG)
+//Comentarios (Client Side)
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product}: HomeProps ) {
   return (
     <>
       <Head>
@@ -21,12 +37,50 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton/>
+          <SubscribeButton priceId={product.priceId}/>
         </section>
         <img src="images/avatar.svg" alt="Girl Coding"/>
       </main>
     </>
   )
+}
+
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const price = await stripe.prices.retrieve('price_1MTWQlD4oAA1Kc4VGEVw1kfj', {
+//     expand: ['product']
+//   })
+
+//   const product = {
+//     priceId: price.id,
+//     amount: new Intl.NumberFormat('en-US', {
+//       style: 'currency',
+//       currency: 'USD',
+//     }).format(price.unit_amount / 100),
+//   };
+//   return {
+//     props: {
+//       product
+//     }
+//   }
+// }
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1MTWQlD4oAA1Kc4VGEVw1kfj', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24, //24 hours
+  }
 }
